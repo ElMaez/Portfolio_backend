400e13694b4095d59ea26764e1b4d2b2eb81ff14
ElMaez <mai_ez@outlook.dk>
2026-06-15 15:11:23 +0200

    new test for deployment of db

---
+++ config.py
@@ -0,0 +1,17 @@
+
+import mysql.connector
+
+
+def db():
+    try:
+        db = mysql.connector.connect(
+            host = "mariadb",
+            user = "root",  
+            password = "password",
+            database = "wash_world"
+        )
+        cursor = db.cursor(dictionary=True)
+        return db, cursor
+    except Exception as e:
+        print(e, flush=True)
+        raise Exception("Database under maintenance", 500)
